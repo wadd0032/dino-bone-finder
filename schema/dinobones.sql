2630414e96eff649e7971469eb19e02ad8c49745
Thomas J Bradley <theman@thomasjbradley.ca>
2012-03-15 19:10:26 -0400

    Updated field order in DB schema.

--- schema/dinobones.sql
+++ schema/dinobones.sql
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `dinobones` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `adr` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `lat` double NOT NULL,
   `lng` double NOT NULL,
-  `adr` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
