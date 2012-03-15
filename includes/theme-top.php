38841c237a8a753d362c6061cffeb8b60a3d4fad
Thomas J Bradley <theman@thomasjbradley.ca>
2012-03-15 19:47:40 -0400

    Updated CSS path.

--- includes/theme-top.php
+++ includes/theme-top.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset=utf-8>
 	<title><?php if (isset($title)) { echo $title . ' · '; } ?>Dinosaurs Bones!</title>
-	<link href="css/public.css" rel="stylesheet">
+	<link href="css/dinobones.css" rel="stylesheet">
 	<script src="js/modernizr.dev.js"></script>
 </head>
 <body>
