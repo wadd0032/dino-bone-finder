9073938cf11c40067698b3b6a6cd3c174b941b89
Thomas J Bradley <theman@thomasjbradley.ca>
2012-03-15 21:12:52 -0400

    Added info window functionality.

--- js/dinobones.js
+++ js/dinobones.js
@@ -11,12 +11,25 @@ $(document).ready(function () {
   // Create a variable to hold the GMap and add the GMap to the page
   var map = new google.maps.Map(document.getElementById('map'), gmapOptions);
 
+  // Create one info window to be shared for all markers
+  var infoWindow;
+
   // Loop through all the places and add a marker to the GMap
   $dinoList.each(function (i, elem) {
     var dino = $(this).find('a').html();
+
+    // Create some HTML content for the info window
+    // Style the content in your CSS
+    var info = '<div class="info-window">'
+      + '<strong>' + dino + '</strong>'
+    ;
+
+    // Determine this dino's latitude and longitude
     var lat = $(this).find('meta[itemprop="latitude"]').attr('content');
     var lng = $(this).find('meta[itemprop="longitude"]').attr('content');
     var pos = new google.maps.LatLng(lat, lng);
+
+    // Create a marker object for this dinosaur
     var marker = new google.maps.Marker({
       position : pos
       , map : map
@@ -24,5 +37,29 @@ $(document).ready(function () {
       , icon : 'images/bone.png'
       , animation: google.maps.Animation.DROP
     });
+
+    // A function for showing this dinosaurs info window
+    function showInfoWindow (ev) {
+      if (ev.preventDefault) {
+        ev.preventDefault();
+      }
+
+      // Close the previous info window first, if one already exists
+      if (infoWindow) {
+        infoWindow.close();
+      }
+
+      // Create an info window object and assign it the content
+      infoWindow = new google.maps.InfoWindow({
+        content : info
+      });
+
+      infoWindow.open(map, marker);
+    }
+
+    // Add a click event listener for the marker
+    google.maps.event.addListener(marker, 'click', showInfoWindow);
+    // Add a click event listener to the list item
+    google.maps.event.addDomListener($(this).children('a').get(0), 'click', showInfoWindow);
   });
 });
