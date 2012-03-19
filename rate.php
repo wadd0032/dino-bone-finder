963a6fa19f275c218842a8e947e00e197ce5208f
Thomas J Bradley <theman@thomasjbradley.ca>
2012-03-19 19:54:07 -0400

    Converted rating scheme to match in class talk.

--- rate.php
+++ rate.php
@@ -11,31 +11,12 @@ if (empty($id) || $rate < 0 || $rate > 5) {
 }
 
 $sql = $db->prepare('
-	SELECT id, votes_num, votes_total
-	FROM dinobones
-	WHERE id = :id
-');
-$sql->bindValue(':id', $id, PDO::PARAM_INT);
-$sql->execute();
-$rating = $sql->fetch();
-$sql = null;
-
-if (empty($rating)) {
-	header('Location: index.php');
-	exit;
-}
-
-$votes_num = $rating['votes_num'] + 1;
-$votes_total = $rating['votes_total'] + $rate;
-
-$sql = $db->prepare('
 	UPDATE dinobones
-	SET votes_num = :votes_num, votes_total = :votes_total
+	SET rate_count = rate_count + 1, rate_total = rate_total + :rate
 	WHERE id = :id
 ');
 $sql->bindValue(':id', $id, PDO::PARAM_INT);
-$sql->bindValue(':votes_num', $votes_num, PDO::PARAM_INT);
-$sql->bindValue(':votes_total', $votes_total, PDO::PARAM_INT);
+$sql->bindValue(':rate', $rate, PDO::PARAM_INT);
 $sql->execute();
 
 header('Location: index.php');
