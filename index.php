ba3b102485c0d802ca8c0e8810ce48d89d6b898a
Thomas J Bradley <theman@thomasjbradley.ca>
2012-03-19 21:08:31 -0400

    Added cookie functinality to disable ratings if previously rated.

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 require_once 'includes/db.php';
 
 $results = $db->query('
-  SELECT id, name, adr, lat, lng, rate_count, rate_total
-  FROM dinobones
-  ORDER BY name ASC
+	SELECT id, name, adr, lat, lng, rate_count, rate_total
+	FROM dinobones
+	ORDER BY name ASC
 ');
 
 include 'includes/theme-top.php';
@@ -14,27 +14,27 @@ include 'includes/theme-top.php';
 
 <ol class="dinos">
 <?php foreach ($results as $dino) : ?>
-  <?php
-    if ($dino['rate_count'] > 0) {
-      $rating = round($dino['rate_total'] / $dino['rate_count']);
-    } else {
-      $rating = 0;
-    }
-  ?>
-  <li itemscope itemtype="http://schema.org/TouristAttraction" data-id="<?php echo $dino['id']; ?>">
-    <a href="single.php?id=<?php echo $dino['id']; ?>" itemprop="name"><?php echo $dino['name']; ?></a>
-    <span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
-      <meta itemprop="latitude" content="<?php echo $dino['lat']; ?>">
-      <meta itemprop="longitude" content="<?php echo $dino['lng']; ?>">
-    </span>
-    <meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter>
-    <ol class="rater">
-    <?php for ($i = 1; $i <= 5; $i++) : ?>
-      <?php $class = ($i <= $rating) ? 'is-rated' : ''; ?>
-      <li class="rater-level <?php echo $class; ?>">★</li>
-    <?php endfor; ?>
-    </ol>
-  </li>
+	<?php
+		if ($dino['rate_count'] > 0) {
+			$rating = round($dino['rate_total'] / $dino['rate_count']);
+		} else {
+			$rating = 0;
+		}
+	?>
+	<li itemscope itemtype="http://schema.org/TouristAttraction" data-id="<?php echo $dino['id']; ?>">
+		<a href="single.php?id=<?php echo $dino['id']; ?>" itemprop="name"><?php echo $dino['name']; ?></a>
+		<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+			<meta itemprop="latitude" content="<?php echo $dino['lat']; ?>">
+			<meta itemprop="longitude" content="<?php echo $dino['lng']; ?>">
+		</span>
+		<meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter>
+		<ol class="rater">
+		<?php for ($i = 1; $i <= 5; $i++) : ?>
+			<?php $class = ($i <= $rating) ? 'is-rated' : ''; ?>
+			<li class="rater-level <?php echo $class; ?>">★</li>
+		<?php endfor; ?>
+		</ol>
+	</li>
 <?php endforeach; ?>
 </ol>
 
--- rate.php
+++ rate.php
@@ -1,16 +1,22 @@
 <?php
 
 require_once 'includes/db.php';
+require_once 'includes/functions.php';
 
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 $rate = filter_input(INPUT_GET, 'rate', FILTER_SANITIZE_NUMBER_INT);
+$cookie = get_rate_cookie();
 
 if (empty($id)) {
 	header('Location: index.php');
 	exit;
 }
 
-if ($rate < 0 || $rate > 5) {
+// Only allow the user to rate if:
+//  1. there is no cookie, aka they haven't already rated
+//  2. the rating value is greater than 0
+//  3. the rating value is less than 5
+if (isset($cookie[$id]) || $rate < 0 || $rate > 5) {
 	header('Location: single.php?id=' . $id);
 	exit;
 }
@@ -24,26 +30,7 @@ $sql->bindValue(':id', $id, PDO::PARAM_INT);
 $sql->bindValue(':rate', $rate, PDO::PARAM_INT);
 $sql->execute();
 
-/*
-  Set a cookie to remember the user has already voted.
-  We have to remember the ID of every single thing they voted on
-   and they must all be inside one single cookie--which is a string.
-  So, we have to come up with a solution to store all the IDs
-   and since we are storing the IDs, we may as well store what they rated.
-
-  // http://www.flickr.com/photos/andyfox/2534644455/sizes/o/in/photostream/
-
-  Our cookie will look something like this:
-   1:4;5:3;6:2
-
-  Or, translated:
-   id:rate;id:rate;id:rate
-*/
-
-$cookie_content = $cookie_content . ';' . $id . ':' . $rate;
-
-// http://php.net/setcookie
-setcookie('dinobones_rated', $cookie_content, time() + 60 * 60 * 24 * 365, '/');
+save_rate_cookie($id, $rate);
 
 header('Location: single.php?id=' . $id);
 exit;
