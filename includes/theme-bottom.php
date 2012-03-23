61d1a563ca129792c2c7daade870b46546524918
wadd0032 <deannawadden@rogers.com>
2012-03-23 10:28:28 -0400

    not sure if this change came from the merge, but I'm committing this anyway

--- includes/theme-bottom.php
+++ includes/theme-bottom.php
@@ -1,5 +1,5 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCOSF6EUJHi28FLeCSkKsQsG1gtn4vRkN4&sensor=false"></script>
+	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyD7ucBegMY84_LCbHdVDeavafwKOBceuAY&sensor=false"></script>
 	<script src="js/dinobones.js"></script>
 </body>
 </html>
